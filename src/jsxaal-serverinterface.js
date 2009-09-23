9240ed42fe3e97c5b87a2514fea95b77db6ad160
vkaravir <vkaravir@7e939fac-81b8-11de-a3c6-f150bf15ed86>
2009-09-23 12:09:21 +0000

    Documented the server interface.

--- src/jsxaal-serverinterface.js
+++ src/jsxaal-serverinterface.js
@@ -1,13 +1,31 @@
+/**
+ * A class used to communicate with a server side when user is interacting
+ * with the animation. This can be adding annotations or answering questions.
+ * @class JSXaal.ServerInterface
+ */
 JSXaal.ServerInterface = Class.create({
 	initialize: function(jsxaalViewer) {
 		this.viewer = jsxaalViewer;
-		console.log("super init");
 	},
+	/**
+	 * This function is called whenever a student answers a question.
+	 * @function {public void} ?
+	 * @param {JSXaal.Question.AbstractQuestion} question - The question that was answered.
+	 */
 	questionAnswered: function(question) {
-		console.log("Unimplemented abstract method JSXaal.ServerInterface.questionAnswered(..)");
+		if (console) {
+			console.log("Unimplemented abstract method JSXaal.ServerInterface.questionAnswered(..)");
+		}
 	},
+	/**
+	 * This function is called whenever a student adds annotations.
+	 * @function {public void} ?
+	 * @param {?} annotation - an XML node describing the annotated polygon element that was drawn.
+	 */
 	annotationAdded: function(annotation) {
-		console.log("Unimplemented abstract method JSXaal.ServerInterface.annotationAdded(..)");
+		if (console) {
+			console.log("Unimplemented abstract method JSXaal.ServerInterface.annotationAdded(..)");
+		}
 	},
 	getViewer: function() {
 		return this.viewer;
